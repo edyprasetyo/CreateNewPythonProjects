b394053dfb114f860b5f3c2508087d8fe9bc33f4
csmcorporatama@gmail.com <indorent10>
2023-03-27 09:55:22 +0700

    update

--- create_new_python_projects.bat
+++ create_new_python_projects.bat
@@ -20,15 +20,19 @@ pip install flask
 :: Create requirements.txt file
 pip freeze > requirements.txt
 
-@echo off
+:: Create .gitignore file
+echo # Python virtual environment > .gitignore
+echo env/ >> .gitignore
+
+:: Create app.py file
 echo from flask import Flask > app.py
-echo app = Flask(__name__) >> app.py
+echo app = Flask(name) >> app.py
 echo. >> app.py
 echo @app.route("/") >> app.py
 echo def hello(): >> app.py
-echo     return "Hello World!" >> app.py
+echo return "Hello World!" >> app.py
 echo. >> app.py
-echo if __name__ == "__main__": >> app.py
-echo     app.run() >> app.py
+echo if name == "main": >> app.py
+echo app.run() >> app.py
 
 echo "Project %project_name% created successfully!"
